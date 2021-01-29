f5132aa894dc4ec448aed73ff3856dd5d34e6811
Shrey Vaity <43662680+Svaity@users.noreply.github.com>
2021-01-28 23:29:15 -0500

    Inorder Postorder and preOrder of BST

---
+++ BST/Traversal.py
@@ -0,0 +1,23 @@
+#Inorder
+def inOrderTraverse(tree, array):
+    if tree:
+		inOrderTraverse(tree.left, array)
+		array.append(tree.value)
+		inOrderTraverse(tree.right, array)
+    return array
+
+#preOrder
+def preOrderTraverse(tree, array):
+    if tree:
+		array.append(tree.value)
+		preOrderTraverse(tree.left, array)		
+		preOrderTraverse(tree.right, array)
+    return array
+
+#PostOrder
+def postOrderTraverse(tree, array):
+    if tree:
+		postOrderTraverse(tree.left, array)
+		postOrderTraverse(tree.right, array)
+		array.append(tree.value)
+    return array
